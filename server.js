f8924916678e4ca52901dd45cb9fafc6875343f8
Amine <aminemouhaouir1996@gmail.com>
2020-12-27 12:10:36 +0100

    2nd commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app=express();
 
-app.use(express.static(__dirname+'/dist/boxoume'));
+app.use(express.static(__dirname+'/dist/angularfirebase-authentication'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/dist/boxoume/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angularfirebase-authentication/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
